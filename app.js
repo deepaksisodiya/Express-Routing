8e0bea6ec3a1c6f4e841db784ae4d81d3b394692
Deepak <deepak.sisodiya@gmail.com>
2015-09-13 01:34:03 +0530

    initial commit

--- app.js
+++ app.js
@@ -18,17 +18,27 @@ adminRouter.use(function (req, res, next) {
   console.log(req.method, req.url);
   next();
 });
-
 // localhost:3000/admin
 adminRouter.get('/', function (req, res) {
   console.log('admin page');
 });
-
 // localhost:3000/admin/users
 adminRouter.get('/users', function (req, res) {
   console.log('admin/users page')
 });
 
+var apiRoutes = express.Router();
+
+// localhost:3000/api
+apiRoutes.get('/', function (req, res) {
+  console.log('mobile API route');
+});
+// localhost:3000/admin/users
+apiRoutes.get('/posts', function (req, res) {
+  console.log('API routes to posts');
+});
+
+app.use('/api', apiRoutes);
 app.use('/admin', adminRouter);
 
 var port = 3000;
